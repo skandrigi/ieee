b92d573cb83ef944581c0e30676808401b74fe22
skandrigi <skandrigi@gmail.com>
2024-10-11 21:29:19 -0500

    updated map

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -1,9 +1,7 @@
 "use client";
-import Image from "next/image";
 import Notifications from "./notifications";
-import MyMap from "./map";
-import { useMemo } from 'react';
 import dynamic from 'next/dynamic';
+import { useMemo } from 'react';
 
 export default function Home() {
   const Map = useMemo(() => dynamic(
@@ -12,11 +10,23 @@ export default function Home() {
       loading: () => <p>A map is loading</p>,
       ssr: false
     }
-  ), [])
+  ), []);
+  
+  const position = [30.6212, -96.3404]; 
+  const zoom = 13; 
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Notifications />
-      <Map />
+    <div className="flex">
+      <div className="bg-slate-200 grid grid-rows-[20px_1fr_20px] w-[200px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="content-center items-center text-center">
+          <Notifications />
+        </div>
+      </div>
+      <div className="w-screen h-[100vh] items-center justify-center">
+        <div className="w-screen h-[100vh]">
+          <Map position={position} zoom={zoom} />
+        </div>
+      </div>
     </div>
   );
 }
