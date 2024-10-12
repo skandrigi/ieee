0357e797dbb3853c880c5c5d301a333e4316c1e7
skandrigi <skandrigi@gmail.com>
2024-10-11 21:04:53 -0500

    update imports

--- my-app/app/map.jsx
+++ my-app/app/map.jsx
@@ -12,9 +12,7 @@ export default function MyMap(props) {
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
     <Marker position={position}>
-      <Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
     </Marker>
   </MapContainer>
 }
